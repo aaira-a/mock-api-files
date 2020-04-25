4868c53961b930772325b38adaf62fe8fdfbd881
azam alias <azamalias@gmail.com>
2020-04-25 18:20:17 +0800

    Change directory before npm install

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,4 +5,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm use 10
+
+cd /home/ubuntu/mock-api-files
 npm install
